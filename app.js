409d56a5bd87ab8c390f846efd66332f76538584
tiouoo <tiouo@qq.com>
2026-04-19 14:45:44 +0800

    feat: 添加CORS中间件以处理跨域请求
    - 引入cors包替代手动设置CORS头
    - 移除自定义CORS中间件代码
    - 更新package.json和package-lock.json添加cors依赖

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ import apiPromise from './src/api/mouter.js';
 import { setupSwagger } from './src/service/swagger.js';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  next();
-});
+app.use(cors());
 
 const filename = fileURLToPath(import.meta.url);
 const _dirname = dirname(filename);
